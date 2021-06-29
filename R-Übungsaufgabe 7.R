11e0d73ad5e8357e8d1d36b59fc999816831eb16
johannesrieder <johannesrieder13@hotmail.de>
2021-06-29 09:04:54 +0200

    Initial Commit

---
+++ R-Übungsaufgabe 7.R
@@ -0,0 +1,73 @@
+#<<<---------- Bemerkung Korrektur --------------->>>
+# -------------
+# Ergebnis:
+# -------------
+# Aufgabe 1: /2
+# Aufgabe 2: /2
+# Aufgabe 3: /4
+# Aufgabe 4: /4
+# Aufgabe 5: /4
+# Aufgabe 6: /4
+# -------------
+# Summe: /20
+#--------------
+#<<<---------------------------------------------->>>
+
+#
+# Template für die Bearbeitung der Übungsaufgaben 7
+#        -- Analyse des BostonHousing-Datensatz -- 
+#
+
+#-----------------
+# Bitte ausfüllen:
+#-----------------
+# Gruppenname:
+# Gruppenteilnehmer: 
+
+
+
+#-------------------------
+# notwendinge Bibliotheken
+#-------------------------
+library(mlbench) #Datensatz
+library(glmnet)  #Ridge/Lasso-Regression
+
+data("BostonHousing")
+
+
+#------------------------------------
+# Aufgabe 1: Trainings- und Testdaten
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+#------------------------------------
+# Aufgabe 2: Lineare Regression
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+#------------------------------------
+# Aufgabe 3: Bias-Variance
+#------------------------------------
+
+#    ...Platz für Ihre Begründung...
+
+#------------------------------------
+# Aufgabe 4: Ridge Regression
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+#------------------------------------
+# Aufgabe 5: Lasso Regression
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+
+#------------------------------------
+# Aufgabe 6: Vergleich der Ergebnisse
+#------------------------------------
+
+#    ...Platz für Ihren Code und Begründung...
