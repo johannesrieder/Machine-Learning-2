48577a11c27b00869c6353ee8c532e492dae8f03
Marius J. Bauer <54984308+mariusjbauer@users.noreply.github.com>
2021-07-02 11:30:56 +0200

    v1

--- R-Übungsaufgabe 7.R
+++ R-Übungsaufgabe 7.R
@@ -31,21 +31,36 @@
 #-------------------------
 library(mlbench) #Datensatz
 library(glmnet)  #Ridge/Lasso-Regression
-
-data("BostonHousing")
-
+library(dplyr)
 
 #------------------------------------
 # Aufgabe 1: Trainings- und Testdaten
 #------------------------------------
+data("BostonHousing")
+data <- BostonHousing
 
-#    ...Platz für Ihren Code...
+set.seed(50)
+# Einteilung des Caravan-Datensatzes in 80% Trainings- und 20% Testdaten
+rows.train <- sample(length(data$medv), 0.8 * length(data$medv))
+train         <- as.data.frame(data[rows.train,])
+test          <- as.data.frame(data[-rows.train,])
+
+# Initialisierung einer Matrizen-Liste für Aufgabe 4
+matrices <- list()
 
 #------------------------------------
 # Aufgabe 2: Lineare Regression
 #------------------------------------
+# Lineare Regression mit Output-Variable Purchase und den anderen Variablen als Input-Variablen
+lm.fit <- lm(formula = medv ~ crim + zn + indus + chas + nox + rm + age + dis + rad + tax + ptratio + b + lstat, 
+             data = train)
 
-#    ...Platz für Ihren Code...
+## Vorhersage der Wahrscheinlichkeiten der Testdaten auf Basis der linearen Regression
+lm_predprob <- predict(object  = lm.fit,
+                       newdata = test,
+                       type    = "response")
+
+# hier fehlt noch die mittlere quadratische Abweichung
 
 #------------------------------------
 # Aufgabe 3: Bias-Variance
@@ -57,15 +72,32 @@ data("BostonHousing")
 # Aufgabe 4: Ridge Regression
 #------------------------------------
 
-#    ...Platz für Ihren Code...
+y <- data$medv
+x <- data %>% select(crim, zn, indus, chas, nox, rm, age, dis, rad, tax, ptratio, b, lstat) %>% data.matrix()
 
+lambda <- 10^seq( from = 5, to = -3, length = 100)
+
+rreg.fit <- glmnet(x = x, 
+                   y = y,
+                   alpha = 0, 
+                   lambda = lambda)
+
+rreg.fit
 #------------------------------------
 # Aufgabe 5: Lasso Regression
 #------------------------------------
 
-#    ...Platz für Ihren Code...
+y <- data$medv
+x <- data %>% select(crim, zn, indus, chas, nox, rm, age, dis, rad, tax, ptratio, b, lstat) %>% data.matrix()
 
+lambda <- 10^seq( from = 5, to = -3, length = 100)
 
+lasreg.fit <- glmnet(x = x, 
+                     y = y,
+                     alpha = 1, 
+                     lambda = lambda)
+
+lasreg.fit
 #------------------------------------
 # Aufgabe 6: Vergleich der Ergebnisse
 #------------------------------------
