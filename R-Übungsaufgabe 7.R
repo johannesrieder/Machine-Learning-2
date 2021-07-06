c6f127bd9ca494626d13fc5998fe752ec3c99920
Marius J. Bauer <54984308+mariusjbauer@users.noreply.github.com>
2021-07-06 20:28:28 +0200

    v5

--- R-Übungsaufgabe 7.R
+++ R-Übungsaufgabe 7.R
@@ -37,7 +37,7 @@ library(dplyr)
 data("BostonHousing")
 data <- BostonHousing
 
-set.seed(50)
+set.seed(42)
 # Einteilung des Caravan-Datensatzes in 80% Trainings- und 20% Testdaten
 rows.train <- sample(length(data$medv), 0.8 * length(data$medv))
 train         <- as.data.frame(data[rows.train,])
@@ -46,10 +46,13 @@ test          <- as.data.frame(data[-rows.train,])
 y <- data.matrix(data$medv)
 x <- data %>% select(crim, zn, indus, chas, nox, rm, age, dis, rad, tax, ptratio, b, lstat) %>% data.matrix()
 
+grid <- 10^seq(from = 5, to = -3, length = 100)
+
 newx.test <- model.matrix(~.-test$medv, 
                           data=test)[,2:14]
 newx.train <- model.matrix(~.-train$medv, 
                            data=train)[,2:14]
+
 #------------------------------------
 # Aufgabe 2: Lineare Regression
 #------------------------------------
@@ -72,60 +75,108 @@ linreg.test.mqa <- mean((test$medv - predict(linreg.fit,newdata=test))^2)
 # Aufgabe 3: Bias-Variance
 #------------------------------------
 
-# Approved by James Jamal Steck's Geodreieck
-# MQA (Varianz) hoch, Bias hoch, Modell nicht gut gefitted, da zu wenig Datenpunkte 
-# --> Underfitting
+# Die MQAs der Trainings- und Testdaten sind sehr hoch, was auf ein Under-
+# fitting hinweist. Underfitting ist in diesem Fall 
+# auf gering verfügbare Trainingsdaten zurückzuführen. Wäre das Modell 
+# hingegen overfitted, würde die MQA bei den Trainingsdaten deutlich geringer 
+# ausfallen als die MQA der Testdaten. 
+#
+# 
 
 #------------------------------------
 # Aufgabe 4: Ridge Regression
 #------------------------------------
 
-ridreg.lambdas <- 10^seq( from = 5, to = -3, length = 100)
 ridreg.cv <- cv.glmnet(x = x, 
                        y = y,
                        alpha = 0, 
-                       lambda = ridreg.lambdas)
+                       lambda = grid)
 
 ridreg.lambda <- ridreg.cv$lambda.min
 
-ridreg.fit <- ridreg.cv$glmnet.fit
-
 ridreg <- glmnet(x, y, alpha = 0, lambda = ridreg.lambda)
 
-ridreg.pred <- predict(ridreg, s = ridreg.lambda, newx = newx.test)
+# Prediction für die Testdaten
+pred <- predict(ridreg, s = ridreg.lambda, newx = newx.test)
 
 # Mittlere quadratische Abweichung für train-Datenpunkte
-ridreg.train.mqa <- mean((train$medv - predict(ridreg.fit,newx=newx.train))^2)
+ridreg.train.mqa <- mean((train$medv - predict(ridreg, newx=newx.train))^2)
 
 # Mittlere quadratische Abweichung für test-Datenpunkte
-ridreg.test.mqa <- mean((test$medv - predict(ridreg.fit,newx=newx.test))^2)
+ridreg.test.mqa <- mean((test$medv - predict(ridreg, newx=newx.test))^2)
+
 
 #------------------------------------
 # Aufgabe 5: Lasso Regression
 #------------------------------------
 
-lasreg.lambdas <- 10^seq( from = 5, to = -3, length = 100)
 lasreg.cv <- cv.glmnet(x = x, 
-                       y = y, 
+                       y = y,
                        alpha = 1, 
-                       lambda = lasreg.lambdas)
+                       lambda = grid)
 
 lasreg.lambda <- lasreg.cv$lambda.min
 
-lasreg.fit <- lasreg.cv$glmnet.fit
+lasreg <- glmnet(x, y, alpha = 1, lambda = lasreg.lambda)
 
-lasreg <- glmnet(x, y, alpha = 0, lambda = lasreg.lambda)
+# Die Parameter "indus" und "age" werden auf 0 gesetzt.
+coef(lasreg)
 
-lasreg.pred <- predict(lasreg, s = lasreg.lambda, newx = newx.test)
+# Prediction für die Testdaten
+pred <- predict(lasreg, s = lasreg.lambda, newx = newx.test)
 
 # Mittlere quadratische Abweichung für train-Datenpunkte
-lasreg.train.mqa <- mean((train$medv - predict(lasreg.fit,newx=newx.train))^2)
+lasreg.train.mqa <- mean((train$medv - predict(lasreg, newx=newx.train))^2)
 
 # Mittlere quadratische Abweichung für test-Datenpunkte
-lasreg.test.mqa <- mean((test$medv - predict(lasreg.fit,newx=newx.test))^2)
+lasreg.test.mqa <- mean((test$medv - predict(lasreg, newx=newx.test))^2)
+
 
 #------------------------------------
 # Aufgabe 6: Vergleich der Ergebnisse
 #------------------------------------
 
-#    ...Platz für Ihren Code und Begründung...
+print("Lineare Regression:")
+print(paste("Train-Fehler: ", linreg.train.mqa))
+print(paste("Test-Fehler: ", linreg.test.mqa))
+print("-------------------------------------")
+print("Ridge Regression:")
+print(paste("Train-Fehler: ", ridreg.train.mqa))
+print(paste("Test-Fehler: ", ridreg.test.mqa))
+print("-------------------------------------")
+print("Lasso Regression:")
+print(paste("Train-Fehler: ", lasreg.train.mqa))
+print(paste("Test-Fehler: ", lasreg.test.mqa))
+print("-------------------------------------")
+
+#
+# Vergleicht man die Modelle anhand des berechneten MQAs, wird ersichtlich, 
+# dass die lineare Regression die geringste MQA aufweisen kann und somit am
+# besten geeignet ist. Die Modelle der Ridge-Regression und Lasso-Regression
+# liefern höhere MQAs.
+# Die Lasso-Regression selektiert Variablen ohne/kaum Einfluss und entfernt
+# diese aus dem Modell. Hingegen werden bei der Ridge-Regression zu große 
+# Koeffizienten verringert, um damit die Vorhersagegenauigkeit der Zielvariable
+# zu verbessern. Da nicht alle Variablen betrachtet werden, wird ein Overfitting
+# der Modelle vermieden, weswegen eine höhere MQA entsteht. Das Modell ist 
+# unserer Ansicht nach underfitted, wodurch die Vorteile der Regularisierung
+# gegen Overfitting nicht greifen.
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
